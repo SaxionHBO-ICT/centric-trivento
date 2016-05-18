e0224d70c6a462d00aa751891a2ad111af03b9bc
Sliomere <kjellspijker@live.nl>
2016-05-18 10:39:24 +0200

    Laatste commit

---
+++ Code/DeventerKroegenApp/app/src/main/java/com/trivento/deventerkroegenapp/activities/LogInActivity.java
@@ -0,0 +1,38 @@
+package com.trivento.deventerkroegenapp.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.trivento.deventerkroegenapp.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final EditText etUsername = (EditText) findViewById(R.id.et_username);
+        final EditText etPassword = (EditText) findViewById(R.id.et_password);
+
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
